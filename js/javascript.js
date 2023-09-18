b1d0bb455617b855b72c4bc8a52e04be0b0d877d
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-09-18 19:51:13 +0300

    Create a dialog form for user to input details for a new book

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,20 @@
+const newBookDialog = document.getElementById("new-book-dialog");
 const btnAddBook = document.querySelector(".add-book");
+const btnCloseDialog = document.getElementById("close-dialog");
+const btnSubmitDialog = document.getElementById("submit-dialog");
 const myLibrary = [];
 
 // Book object constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read
+  this.status = status
 }
 
 // Get user's input about the book, create a new book object and store it into the array
-function addBookToLibrary() {
-  const title = prompt("Enter the title of the book:");
-  const author = prompt("Enter the name of the author:");
-  const pages = prompt("Enter the number of pages:");
-  const read = prompt("Have you read the book yet?")
-  const book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, status) {
+  const book = new Book(title, author, pages, status);
   myLibrary.push(book);
 }
 
@@ -23,11 +22,15 @@ function addBookToLibrary() {
 function displayBooks() {
   console.log(myLibrary);
   for (let i = 0; i < myLibrary.length; i++) {
-    // Display the books title
+    // Remove previous entries
     const bookCard = document.querySelector(`.books div:nth-of-type(${i + 1})`);
-    const h3Element = document.createElement("h3");
-    h3Element.textContent = myLibrary[i].title;
-    bookCard.appendChild(h3Element);
+    while (bookCard.firstChild) {
+      bookCard.removeChild(bookCard.firstChild);
+    }
+    // Display the books title
+    const h4Element = document.createElement("h4");
+    h4Element.textContent = myLibrary[i].title;
+    bookCard.appendChild(h4Element);
     // Display the books author
     const paraAuthor = document.createElement("p");
     paraAuthor.textContent = `Author: ${myLibrary[i].author}`;
@@ -37,12 +40,13 @@ function displayBooks() {
     paraPages.textContent = `Number of pages: ${myLibrary[i].pages}`;
     bookCard.appendChild(paraPages);
     // Display read status
-    const paraRead = document.createElement("p");
-    paraRead.textContent = myLibrary[i].read;
-    bookCard.appendChild(paraRead);
+    const paraStatus = document.createElement("p");
+    paraStatus.textContent = `Status: ${myLibrary[i].status}`;
+    bookCard.appendChild(paraStatus);
   }
 }
 
+// Creates a new book card
 function createNewBookCard() {
   const booksGrid = document.querySelector(".books");
   const bookElement = document.createElement("div");
@@ -50,13 +54,37 @@ function createNewBookCard() {
   booksGrid.insertBefore(bookElement, btnAddBook);
 }
 
-const book1 = new Book("Stories of the Macabre", "H.P. Lovecraft", 158, "not read yet");
+const book1 = new Book("Stories of the Macabre", "H.P. Lovecraft", 158, "not read");
 myLibrary.push(book1);
 
-const book2 = new Book("Eisenhorn The Omnibus - Warhammer 40,000", "Dan Abnett", 944, "not read yet");
-myLibrary.push(book2)
+const book2 = new Book("Eisenhorn The Omnibus - Warhammer 40,000", "Dan Abnett", 944, "not read");
+myLibrary.push(book2);
 
 
 displayBooks();
 
-btnAddBook.addEventListener("click", createNewBookCard);
+
+btnAddBook.addEventListener("click", () => {
+  newBookDialog.showModal();
+});
+
+btnSubmitDialog.addEventListener("click", () => {
+  const userInputTitle = document.getElementById("title").value;
+  const userInputAuthor = document.getElementById("author").value;
+  const userInputPages = document.getElementById("pages").value;
+  const statusCheckbox = document.getElementById("status");
+  let userInputStatus;
+  if (statusCheckbox.checked) {
+    userInputStatus = "read";
+  } else {
+    userInputStatus = "not read"
+  };
+
+  createNewBookCard();
+  addBookToLibrary(userInputTitle, userInputAuthor, userInputPages, userInputStatus)
+  displayBooks();
+});
+
+btnCloseDialog.addEventListener("click", () => {
+  newBookDialog.close();
+});
